6aa4f26b50c9e9c4a640a5708ab71468607be88b
nils <slincire@gmail.com>
2016-05-20 15:57:22 -0400

    added cube model. setup basic scene with rotating cube

--- core/src/com/hh/gdxtutorial/Game.java
+++ core/src/com/hh/gdxtutorial/Game.java
@@ -9,48 +9,78 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
 public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	private PerspectiveCamera camera;
+	public PerspectiveCamera camera;
 
-	private AssetManager assetManager;
-	private Array<ModelInstance> instances = new Array<ModelInstance>();
+	public AssetManager assetManager;
+	public Array<ModelInstance> instances = new Array<ModelInstance>();
 
-	private FrameBuffer fbo;
-	private TextureRegion textureRegion;
-	private ShaderProgram lineShader = new CelLineShaderProgram();
+	public FrameBuffer fbo;
+	public TextureRegion textureRegion;
 
-	private SpriteBatch spriteBatch = new SpriteBatch();
-//	private ModelBatch modelBatch = new ModelBatch(Gdx.files.classpath("com/badlogic/gdx/graphics/g3d/shaders/default.vertex.glsl").readString(), Gdx.files.internal("shaders/cel.main.fragment.glsl").readString());
-//	private ModelBatch depthBatch = new ModelBatch(new CelDepthShaderProvider());
-	private Environment environment = new Environment();
+	public SpriteBatch spriteBatch;
+	public ModelBatch modelBatch;
 
+	public Environment environment = new Environment();
+
+	public CameraInputController camController;
+
+	public boolean loading = true;
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		modelBatch = new ModelBatch();
+
 		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(0, 5, 5);
+		camera.lookAt(0, 0, 0);
+		camera.near = 1;
+		camera.far = 1000;
+		camera.update();
+
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 1.8f, -1f, -0.8f, 0.2f));
+
+		camController = new CameraInputController(camera);
+		Gdx.input.setInputProcessor(camController);
+
 		assetManager = new AssetManager();
+		assetManager.load("models/cube.g3dj", Model.class);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		final float delta = Math.min(1/30f, Gdx.graphics.getDeltaTime());
+		camera.update();
+		camController.update();
+
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		if (loading && assetManager.update())
+			doneLoading();
+
+		for(ModelInstance instance: instances)
+			instance.transform.rotate(new Vector3(0, 1, 0), (90 * delta) % 360);
+
+		modelBatch.begin(camera);
+		modelBatch.render(instances, environment);
+		modelBatch.end();
 	}
 
 	@Override
 	public void resize (int width, int height) {
+		camera.viewportWidth = width;
+		camera.viewportHeight = height;
+		camera.update();
 	}
 
 	@Override
@@ -63,5 +93,14 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void dispose () {
+		modelBatch.dispose();
+		spriteBatch.dispose();
+		assetManager.dispose();
+	}
+	public void doneLoading() {
+		loading = false;
+		ModelInstance instance = new ModelInstance(assetManager.get("models/cube.g3dj", Model.class));
+		instance.transform.setTranslation(0.0f, 0.0f, 0.0f);
+		instances.add(instance);
 	}
 }
