1d2277b197d4337bd36ede4e43348cb98afb4532
nils <slincire@gmail.com>
2016-05-20 18:09:12 -0400

    commented and cleaned up the basic setup

--- core/src/com/hh/gdxtutorial/Game.java
+++ core/src/com/hh/gdxtutorial/Game.java
@@ -5,17 +5,12 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.glutils.FrameBuffer;
-import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 
@@ -25,57 +20,64 @@ public class Game extends ApplicationAdapter {
 	public AssetManager assetManager;
 	public Array<ModelInstance> instances = new Array<ModelInstance>();
 
-	public FrameBuffer fbo;
-	public TextureRegion textureRegion;
-
-	public SpriteBatch spriteBatch;
 	public ModelBatch modelBatch;
 
-	public Environment environment = new Environment();
+	public Environment environment;
 
 	public CameraInputController camController;
 
 	public boolean loading = true;
+
 	@Override
 	public void create () {
-		modelBatch = new ModelBatch();
-
+		// declare and configure the camera.
 		camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		camera.position.set(0, 5, 5);
 		camera.lookAt(0, 0, 0);
 		camera.near = 1;
 		camera.far = 1000;
 		camera.update();
-
-		environment.add(new DirectionalLight().set(0.8f, 0.8f, 1.8f, -1f, -0.8f, 0.2f));
-
+		// declare camController and set it as the input processor.
 		camController = new CameraInputController(camera);
 		Gdx.input.setInputProcessor(camController);
-
+		// declare the modelBatch and environment, add a light to the environment.
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 1.8f, -1f, -0.8f, 0.2f));
+		// declare the assetManager and load the model.
 		assetManager = new AssetManager();
 		assetManager.load("models/cube.g3dj", Model.class);
+		// set the clear color
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 	}
 
 	@Override
 	public void render () {
+		// cache delta
 		final float delta = Math.min(1/30f, Gdx.graphics.getDeltaTime());
-		camera.update();
+		// update the camController (this also updates the camera).
 		camController.update();
-
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		// clear color and depth buffers.
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+		// trigger done loading when the assets are loaded.
 		if (loading && assetManager.update())
 			doneLoading();
 
+		// set model rotations.
 		for(ModelInstance instance: instances)
 			instance.transform.rotate(new Vector3(0, 1, 0), (90 * delta) % 360);
 
+		// render the scene
 		modelBatch.begin(camera);
 		modelBatch.render(instances, environment);
 		modelBatch.end();
 	}
 
+	/*
+	 * On screen/window resize:
+	 * Reset the camera viewportWidth and viewportHeight
+	 */
 	@Override
 	public void resize (int width, int height) {
 		camera.viewportWidth = width;
@@ -84,19 +86,13 @@ public class Game extends ApplicationAdapter {
 	}
 
 	@Override
-	public void pause () {
-	}
-
-	@Override
-	public void resume () {
-	}
-
-	@Override
 	public void dispose () {
 		modelBatch.dispose();
-		spriteBatch.dispose();
 		assetManager.dispose();
 	}
+	/*
+	 * Add modelInstances to the scene once all assets are done loading.
+	 */
 	public void doneLoading() {
 		loading = false;
 		ModelInstance instance = new ModelInstance(assetManager.get("models/cube.g3dj", Model.class));
