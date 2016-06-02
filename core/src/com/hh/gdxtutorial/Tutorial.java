912706224226420fe2ffdf98d5abb82de5f5521a
nils <slincire@gmail.com>
2016-06-02 14:48:46 -0400

    set screen to TurnEngineScreen on default

--- core/src/com/hh/gdxtutorial/Tutorial.java
+++ core/src/com/hh/gdxtutorial/Tutorial.java
@@ -2,15 +2,17 @@ package com.hh.gdxtutorial;
 
 import com.badlogic.gdx.Game;
 import com.hh.gdxtutorial.screens.CelShaderScreen;
+import com.hh.gdxtutorial.screens.GaussianBlurShaderScreen;
+import com.hh.gdxtutorial.screens.TiltShiftShaderScreen;
 import com.hh.gdxtutorial.screens.TurnEngineScreen;
 
 public class Tutorial extends Game {
 
 	@Override
 	public void create () {
-		setScreen(new CelShaderScreen());
+//		setScreen(new CelShaderScreen());
 //		setScreen(new GaussianBlurShaderScreen());
-//		setScreen(new TurnEngineScreen());
+		setScreen(new TurnEngineScreen());
 //		setScreen(new TiltShiftShaderScreen());
 	}
 
