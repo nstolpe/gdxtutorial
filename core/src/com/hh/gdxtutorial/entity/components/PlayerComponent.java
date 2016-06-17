a160c93e98519106b9a73bac3c8e548fec6f405e
nils <slincire@gmail.com>
2016-06-17 14:01:44 -0400

    added a comment

--- core/src/com/hh/gdxtutorial/entity/components/PlayerComponent.java
+++ core/src/com/hh/gdxtutorial/entity/components/PlayerComponent.java
@@ -3,7 +3,8 @@ package com.hh.gdxtutorial.entity.components;
 import com.badlogic.ashley.core.Component;
 
 /**
- * Created by nils on 6/5/16.
+ * An empty component that indicates an Entity is a player.
+ * Could provide something else later.S
  */
 public class PlayerComponent implements Component {
 }
