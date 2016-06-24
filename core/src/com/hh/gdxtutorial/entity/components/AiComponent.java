9807aa7b2c01c36c89723aef9c152a0d80eaabed
nils <nils@gangshi>
2016-06-24 13:49:39 -0400

    Added state machine to ai component

--- core/src/com/hh/gdxtutorial/entity/components/AiComponent.java
+++ core/src/com/hh/gdxtutorial/entity/components/AiComponent.java
@@ -1,17 +1,106 @@
 package com.hh.gdxtutorial.entity.components;
 
 import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.gdx.ai.fsm.DefaultStateMachine;
+import com.badlogic.gdx.ai.fsm.State;
+import com.badlogic.gdx.ai.fsm.StateMachine;
+import com.badlogic.gdx.ai.msg.Telegram;
 
 /**
  * Created by nils on 6/5/16.
  */
 public class AiComponent implements Component {
-	public AiComponent () {}
+	public StateMachine<AiComponent, MobState> stateMachine;
 
-	/**
-	 * Randomly moves in a set area. This is a stub.
-	 */
-	public void run() {
+	public AiComponent () {
+		stateMachine = new DefaultStateMachine<AiComponent, MobState>(this, MobState.REST);
+	}
 
+	public static enum MobState implements State<AiComponent> {
+		REST() {
+			@Override
+			public void enter(AiComponent entity) {
+
+			}
+
+			@Override
+			public void update(AiComponent entity) {
+
+			}
+
+			@Override
+			public void exit(AiComponent entity) {
+
+			}
+
+			@Override
+			public boolean onMessage(AiComponent entity, Telegram telegram) {
+				return false;
+			}
+		},
+		ATTACK() {
+			@Override
+			public void enter(Entity entity) {
+
+			}
+
+			@Override
+			public void update(Entity entity) {
+
+			}
+
+			@Override
+			public void exit(Entity entity) {
+
+			}
+
+			@Override
+			public boolean onMessage(Entity entity, Telegram telegram) {
+				return false;
+			}
+		},
+		ATTACK_PRE() {
+			@Override
+			public void enter(Entity entity) {
+
+			}
+
+			@Override
+			public void update(Entity entity) {
+
+			}
+
+			@Override
+			public void exit(Entity entity) {
+
+			}
+
+			@Override
+			public boolean onMessage(Entity entity, Telegram telegram) {
+				return false;
+			}
+		},
+		ATTACK_POST() {
+			@Override
+			public void enter(Entity entity) {
+
+			}
+
+			@Override
+			public void update(Entity entity) {
+
+			}
+
+			@Override
+			public void exit(Entity entity) {
+
+			}
+
+			@Override
+			public boolean onMessage(Entity entity, Telegram telegram) {
+				return false;
+			}
+		}
 	}
 }
