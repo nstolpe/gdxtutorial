c92a6b1a13d593806fda693d12237568a5a30072
nils <slincire@gmail.com>
2016-06-28 14:49:27 -0400

    got rotations working for the edge cases. finally

--- core/src/com/hh/gdxtutorial/helpers/Utility.java
+++ core/src/com/hh/gdxtutorial/helpers/Utility.java
@@ -12,23 +12,34 @@ public class Utility {
 	 * @param origin
 	 * @param target
 	 * @return
-	 * @TODO Make sure those epsilon values are working. Troubleshoot the instances where
-	 * things move backwards. Use different axis is z won't work.
+	 * @TODO Make sure those epsilon values are working.
 	 */
-	public static Quaternion getRotationTo(Vector3 origin, Vector3 target, Quaternion fallback) {
+	public static Quaternion getRotationTo(Vector3 origin, Vector3 target, Quaternion rotation) {
+		// http://stackoverflow.com/questions/29686149/find-axis-angle-of-one-vector3-relative-to-another
 		// Get the difference between the two points.
 		Vector3 difference = target.cpy().sub(origin);
+
+		// if difference is too small, return the current rotation quat
+		// epsilon here might be way too small.
+		if (difference.len() < 0.000000001f) return new Quaternion(rotation);
+
 		Vector3 direction = difference.cpy().nor();
 
-		if (difference.len() < 0.000000001f) return new Quaternion(fallback);
-
 		// z is forward
-		Vector3 zAxis = new Vector3(0,0,1);
-
+		Vector3 zAxis = new Vector3(Vector3.Z);
 		float dot = zAxis.dot(direction);
+
+		// if the target point is positive Z from origin, return a new quat
+		if(dot == 1) return new Quaternion();
+		// if the target point is negative Z from the origin, rotate 180 on up axis (Y in this case).
+		if (dot == -1) return new Quaternion(Vector3.Y, 180);
+
+		// get angle between vectors
 		float angle = (float) Math.acos(dot);
 
-		if (angle < 0.000000001f) return new Quaternion(fallback);
+		// if the angle is too small, return current rotation quat
+		// as above, epsilon is probably too small.
+		if (angle < 0.000000001f) return new Quaternion(rotation);
 
 		return new Quaternion().setFromCross(zAxis, direction);
 	}
