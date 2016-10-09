633c9026a7f8f3fbf1706cc2510689631150aef5
nils <slincire@gmail.com>
2016-10-09 17:49:34 -0400

    added AnimationControllerComponent. first step in moving animations to own system

---
+++ core/src/com/hh/ghostengine/entity/components/AnimationControllerComponent.java
@@ -0,0 +1,11 @@
+package com.hh.ghostengine.entity.components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
+
+/**
+ * Created by nils on 10/9/16.
+ */
+public class AnimationControllerComponent implements Component {
+	public AnimationController controller;
+}
