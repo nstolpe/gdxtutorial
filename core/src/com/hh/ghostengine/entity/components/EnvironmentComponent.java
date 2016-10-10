d78fbe53ad67ae05a0f21624eac437b68f59c944
nils <slincire@harpoonandhatchet.com>
2016-10-10 02:54:24 -0400

    Added body to environment component

--- core/src/com/hh/ghostengine/entity/components/EnvironmentComponent.java
+++ core/src/com/hh/ghostengine/entity/components/EnvironmentComponent.java
@@ -1,7 +1,15 @@
 package com.hh.ghostengine.entity.components;
 
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g3d.Environment;
+
 /**
  * Created by nils on 10/10/16.
  */
-public class EnvironmentComponent {
+public class EnvironmentComponent implements Component {
+	public Environment environment;
+
+	public EnvironmentComponent(Environment environment) {
+		this.environment = environment;
+	}
 }
