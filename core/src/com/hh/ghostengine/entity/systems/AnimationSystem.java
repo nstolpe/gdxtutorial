49f695da39f3f8e1445aed396436bd940e63c454
nils <slincire@gmail.com>
2016-11-03 00:39:36 -0400

    Added animationsystem stub

---
+++ core/src/com/hh/ghostengine/entity/systems/AnimationSystem.java
@@ -0,0 +1,14 @@
+package com.hh.ghostengine.entity.systems;
+
+import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.EntitySystem;
+
+/**
+ * Created by nils on 10/10/16.
+ */
+public class AnimationSystem extends EntitySystem {
+	@Override
+	public void addedToEngine(Engine engine) {
+
+	}
+}
